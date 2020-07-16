b4c8c17ef104709bbea1bd161dec2bb76577f829
Krish Agarwal <akrish136@gmail.com>
2020-07-16 06:07:46 +0530

    Add comments

--- scripts/parse.py
+++ scripts/parse.py
@@ -65,7 +65,7 @@ tabb['fasta_record'] = []
 filenameparam = str(options.filename)
 
 dirrs = options.indir
-dirrs = sorted(dirrs)
+dirrs = sorted(dirrs) # sorting so that we dont have any random order
 outdir = str(options.outfile)
 
 rel_outfile = os.path.relpath(outdir)
@@ -73,48 +73,35 @@ rel_outfile_csv = os.path.join(
     os.path.dirname(rel_outfile),
     "combined_MotEvo_results.csv")
 
-ii = -1
 for dirr in dirrs:
     abs_path = os.path.join(dirr, filenameparam)
 
     filename = os.path.relpath(abs_path)
 
-    i = 0
-    j = 0
+    i = 0 # this variable will check whether we are at the first line of the record or second
     file = open(filename, "r")
     for each in file:
         each_word = each.split(' ')
-        if(i % 2 == 0):
-            tabb['binding_position'] = tabb['binding_position'] + [each_word[0]]
+        if(i % 2 == 0): # first line 
+            tabb['binding_position'] = tabb['binding_position'] + [each_word[0]] 
             tabb['strand'] = tabb['strand'] + [each_word[1]]
             tabb['binding_posterior'] = tabb['binding_posterior'] + [each_word[2]]
             tabb['pwm_id'] = tabb['pwm_id'] + [each_word[3]]
             tabb['binding_region'] = tabb['binding_region'] + [each_word[4]]
-        else:
+        else: # second line 
             tabb['binding_sequence'] = tabb['binding_sequence'] + [each_word[0]]
             tabb['binding_energy'] = tabb['binding_energy'] + [each_word[1]]
             tabb['fasta_record'] = tabb['fasta_record'] + [each_word[2]]
         i = i + 1
 
-list_items = ['pwm_id', 'binding_position', 'binding_sequence', 'binding_posterior', 'binding_energy'] #these will be the headers in the final tsv file
+list_items = ['pwm_id', 'binding_position', 'binding_sequence', 'binding_posterior', 'binding_energy'] # these will be the headers in the final tsv file
 
-# df = pd.DataFrame({key: pd.Series(value) for key, value in tabb.items()})
-# df.to_csv(rel_outfile_csv, index=False)
-tempDict1 = {}
+tempDict1 = {} # initializing empty dictionaries
 for item in list_items:
-    tempDict = {item : pd.Series(tabb[item])}
-    tempDict1.update(tempDict)
+    tempDict = {item : pd.Series(tabb[item])} # appending list of values for each key 
+    tempDict1.update(tempDict) # concating dictionary
 df = pd.DataFrame(tempDict1)
-print(tempDict1)
-df.to_csv(rel_outfile, index=False, sep='\t', header=list_items) 
-
-
-# with open(rel_outfile_csv, 'r') as csvin, open(rel_outfile, 'w') as tsvout:
-#     csvin = csv.reader(csvin)
-#     tsvout = csv.writer(tsvout, delimiter='\t')
-
-#     for row in csvin:
-#         tsvout.writerow(row) #printing row by row in tsv
+df.to_csv(rel_outfile, index=False, sep='\t', header=list_items) # creating a tsv file
 
 
 
