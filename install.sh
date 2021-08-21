73cdc499ab1001059b0204aea9649bdf8c2104a2
Maciej Bak <wsciekly.maciek@gmail.com>
2021-08-21 23:06:27 +0200

    ci: test

--- install.sh
+++ install.sh
@@ -15,27 +15,33 @@ CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 echo $CWD
 
 # remove git repository
+echo "[0/7]: Removing .git internal directory..."
 rm -rf .git
 
 # create and activate main bindz conda env
-echo "[1/3]: Creating main conda env for bindz..."
+echo "[1/7]: Creating main conda env for bindz..."
 conda env create --file envs/main.yml
+SHELLNAME=$(echo $0 | sed 's|-||g')
+conda init $SHELLNAME
 conda activate bindz
 conda list
 
-echo "[2/3]: Building all workflow-specific conda envs..."
+echo "[2/7]: Building all workflow-specific conda envs..."
 
-echo "[3/3]: Parsing ATtRACT db..."
+echo "[3/7]: Parsing ATtRACT db..."
 
-echo "[4/3]: Generating sequence logos..."
+echo "[4/7]: Generating sequence logos..."
 
-# populate config with paths to repo
+echo "[5/7]: Adjusting config template..."
 # sed?
 
+echo "[6/7]: Adjusting \$PATH..."
 # hook up /bin/bindz to PATH
 # separate for mac, linux
 
-# test exection?
-# bindz
+echo "[7/7]: Testing installation..."
+bindz
+
+echo "SUCCESS!"
 
 # print error msg on error!
