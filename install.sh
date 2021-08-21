5f02b46b3878a3603b43dce069406e57c57aad63
Maciej Bak <wsciekly.maciek@gmail.com>
2021-08-22 01:34:51 +0200

    ci: test

--- install.sh
+++ install.sh
@@ -25,11 +25,11 @@ REPODIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # remove git repository
 echo "[0/7]: Removing .git internal directory..."
-#rm -rf .git
+rm -rf .git
 
 # create and activate main bindz conda env
 echo "[1/7]: Creating main conda env for bindz..."
-#conda env create --file envs/main.yml --quiet
+conda env create --file envs/main.yml --quiet
 SHELLNAME=$(echo $SHELL | rev | cut -d '/' -f1 | rev)
 echo $SHELLNAME
 export PS1=
